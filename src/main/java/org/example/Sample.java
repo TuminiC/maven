84edeadefb233f948078448583e9d5d489d4bce7
TuminiC <tumini.cookey@mail.mcgill.ca>
2023-11-26 19:04:57 -0500

    adding floating and double values

---
+++ src/main/java/org/example/Sample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Sample {
+    public static void main(String[] args) {
+        float max = 3.40282346638528860e+38f;
+        float min = 1.40129846432481707e-45f;
+        double doubleMax = 3.40282346638528860e+38d;
+        double doubleMin = 4.94065645841246544e-324d;
+        System.out.println(doubleMax);
+        System.out.printf("%.2f",max);
+    }
+}
